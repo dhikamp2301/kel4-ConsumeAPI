b76c5481601a99bed5f2088dae5dcd4acc70df50
dhikamp2301 <2006070@itg.ac.id>
2023-06-14 20:20:29 -0700

    home_page.dart

---
+++ lib/utils/endpoint.dart
@@ -0,0 +1,11 @@
+import 'dart:html';
+
+class EndPoint {
+  static const _baseUrl = "https://jsonplaceholder.typicode.com/todos/1";
+  static const post = "$_baseUrl/posts";
+  static const comment = "$_baseUrl/comments";
+  static const album = "$_baseUrl/albums";
+  static const photo = "$_baseUrl/photos";
+  static const todo = "$_baseUrl/todos";
+  static const user = "$_baseUrl/users";
+}
