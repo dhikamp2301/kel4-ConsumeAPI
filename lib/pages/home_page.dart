af056794d106c7294b29b2f73d964657b9ed0ed9
bintangnuralfiyah <2006191@itg.ac.id>
2023-06-15 02:21:26 -0700

    post_services.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:pertemuan_v/controllers/post_controller.dart';
+
+import '../models/post.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,13 +13,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final PostController postController = PostController();
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Page"),
       ),
-      body: const SafeArea(child: Center()),
+      body: SafeArea(
+        child: FutureBuilder<List<Post>>(
+          future: postController.fetchAll(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData) {
+              if (snapshot.data!.isNotEmpty) {
+                inspect(snapshot.data!);
+                return ListView.separated(
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return SizedBox(
+                      height: size.height * 0.01,
+                    );
+                  },
+                  itemCount: snapshot.data!.length,
+                );
+              } else {
+                return Text("tidak ada data");
+              }
+            } else {
+              return Text("err");
+            }
+          },
+        ),
+      ),
     );
   }
 }
--- lib/services/post_service.dart
+++ lib/services/post_service.dart
@@ -34,4 +34,41 @@ class PostService {
     return await http.post(url,
         body: jsonEncode(jsonBody), headers: RequestHelper.basicHeader());
   }
+
+  Future<http.Response> put(Post post) async {
+    String endPoint = "${EndPoint.post}/${post.id}";
+    Uri url = Uri.file(endPoint);
+    var jsonBody = post.toMap();
+
+    return await http.put(
+      url,
+      body: jsonEncode(jsonBody),
+      headers: RequestHelper.basicHeader(),
+    );
+  }
+
+  Future<http.Response> patch(
+      {required int id, String? title, String? body}) async {
+    String endPoint = "${EndPoint.post}/$id";
+    Uri url = Uri.file(endPoint);
+
+    Map<String, dynamic> jsonBody = {};
+    if (title != null) {
+      jsonBody["title"] = title;
+    }
+
+    if (body != null) {
+      jsonBody["body"] = body;
+    }
+
+    return await http.patch(url,
+        body: jsonEncode(jsonBody), headers: RequestHelper.basicHeader());
+  }
+
+  Future<http.BaseResponse> delete(int id) async {
+    String endPoint = "${EndPoint.post}/$id";
+    Uri url = Uri.file(endPoint);
+
+    return await http.delete(url, headers: RequestHelper.basicHeader());
+  }
 }
