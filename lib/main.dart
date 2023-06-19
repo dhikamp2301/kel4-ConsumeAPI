65d3cac31622b3850da109d7d1ea1983b3a61da6
dhikamp2301 <2006070@itg.ac.id>
2023-06-19 21:58:37 +0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:costume_api/view/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:pertemuan_v/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,14 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Kel 4 ConsumeAPI',
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
-      routeInformationParser: AppRoutes.goRouter.routeInformationParser,
-      routeInformationProvider: AppRoutes.goRouter.routeInformationProvider,
-      routerDelegate: AppRoutes.goRouter.routerDelegate,
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
