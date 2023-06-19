3cbe4cc4f422def04c4f74a38ed709a37e0ee72c
dhikamp2301 <2006070@itg.ac.id>
2023-06-19 22:00:36 +0700

    widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pertemuan_v/main.dart';
+import 'package:costume_api/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
